a3b5ad6e5cf220f7abd1ac962530d1da8a70c60c
nayf56706-spec <nayf56706@gmail.com>
2026-08-09 21:16:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,17 @@
 // ==========================================
 
 require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Server is active!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
 const { 
     Client, 
     GatewayIntentBits, 
